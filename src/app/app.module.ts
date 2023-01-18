e4ad4ec13c0af4d817dc5c8ccb3043671bd2b0b9
itsmehrd <kingharish22@gmail.com>
2023-01-18 13:55:09 +0530

    Commit-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MasterHeaderComponent } from './master-header/master-header.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { PortfolioModalsComponent } from './portfolio-modals/portfolio-modals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MasterHeaderComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    CopyrightComponent,
+    PortfolioModalsComponent
   ],
   imports: [
     BrowserModule,
